fbbc2be184e4d84a2b2e5498f961d3d8e3f956fa
psnjit <prasenjitsaha49@gmail.com>
2024-05-15 10:25:19 +0530

    Adding refresh button and removing auto refresh

--- script.js
+++ script.js
@@ -1,6 +1,9 @@
 const API_KEY= "CG-JZSUUjDAFzYB9e1uKUUjbeqG";
 const URL = 'https://api.coingecko.com/api/v3/simple/price?ids=bitcoin%2Cethereum%2Cdogecoin&vs_currencies=inr';
+const PROXY_URL = 'https://cors-anywhere.herokuapp.com/' + URL;
+
 const pricePoints = document.querySelectorAll('h3');
+const refreshBtn= document.querySelector('.refresh');
 
 async function setData(){
     const response= await fetch(URL);
@@ -10,5 +13,6 @@ async function setData(){
     pricePoints[1].innerHTML=data.ethereum.inr+' INR';
     pricePoints[2].innerHTML=data.dogecoin.inr+' INR';
 }
+setData();
 
-setInterval(setData, 1000);
+refreshBtn.addEventListener('click', setData);
